02152310d987ad3259b93a13a45f4a78e6cf6d98
Sylvain Rougié <46543145+Sipixer@users.noreply.github.com>
2025-01-22 17:24:01 +0100

    change title seo index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,64 +1,64 @@
 import type { LoaderFunction, MetaFunction } from "@remix-run/cloudflare";
-import { Layout } from "~/components/Layout";
-import { HeroSection } from "~/components/HeroSection";
-import { ProductSection } from "~/components/ProductSection";
-import { RetroExperienceSection } from "~/components/RetroExperienceSection";
-import { CTASection } from "~/components/CTASection";
-import { TestimonialSection } from "~/components/TestimonialSection";
-import { ContactForm } from "~/components/ContactForm";
-import { and, desc, eq, InferSelectModel } from "drizzle-orm";
+import { useLoaderData } from "@remix-run/react";
+import { type InferSelectModel, and, desc, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/d1";
 import { radioTable } from "src/db/schema";
-import { useLoaderData } from "@remix-run/react";
+import { CTASection } from "~/components/CTASection";
+import { ContactForm } from "~/components/ContactForm";
+import { HeroSection } from "~/components/HeroSection";
+import { Layout } from "~/components/Layout";
+import { ProductSection } from "~/components/ProductSection";
+import { RetroExperienceSection } from "~/components/RetroExperienceSection";
+import { TestimonialSection } from "~/components/TestimonialSection";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Novelum - Radios Vintage Réinventées" },
-    {
-      name: "description",
-      content:
-        "Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
-    },
-    { property: "og:title", content: "Novelum - Radios Vintage Réinventées" },
-    {
-      property: "og:description",
-      content:
-        "Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
-    },
-    { property: "og:url", content: "https://novelum-radio.fr/" },
-    { property: "og:type", content: "website" },
-    { property: "og:locale", content: "fr_FR" },
-    { name: "twitter:title", content: "Novelum - Radios Vintage Réinventées" },
-    {
-      name: "twitter:description",
-      content:
-        "Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
-    },
-  ];
+	return [
+		{ title: "Novelum - Radios Vintage Modernisées" },
+		{
+			name: "description",
+			content:
+				"Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
+		},
+		{ property: "og:title", content: "Novelum - Radios Vintage Modernisées" },
+		{
+			property: "og:description",
+			content:
+				"Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
+		},
+		{ property: "og:url", content: "https://novelum-radio.fr/" },
+		{ property: "og:type", content: "website" },
+		{ property: "og:locale", content: "fr_FR" },
+		{ name: "twitter:title", content: "Novelum - Radios Vintage Modernisées" },
+		{
+			name: "twitter:description",
+			content:
+				"Découvrez Novelum, où les radios vintage retrouvent vie grâce à une restauration artisanale et des technologies modernes. Un mariage unique d'authenticité et d'innovation.",
+		},
+	];
 };
 export const loader: LoaderFunction = async ({ context }) => {
-  const env = context.cloudflare.env as Env;
-  const db = drizzle(env.DB);
-  const radios = await db
-    .select()
-    .from(radioTable)
-    .where(and(eq(radioTable.public, true), eq(radioTable.is_sold, false)))
-    .orderBy(desc(radioTable.created_at))
-    .limit(4);
-  return Response.json(radios);
+	const env = context.cloudflare.env as Env;
+	const db = drizzle(env.DB);
+	const radios = await db
+		.select()
+		.from(radioTable)
+		.where(and(eq(radioTable.public, true), eq(radioTable.is_sold, false)))
+		.orderBy(desc(radioTable.created_at))
+		.limit(4);
+	return Response.json(radios);
 };
 type RadioModel = InferSelectModel<typeof radioTable>;
 
 export default function Index() {
-  const radios = useLoaderData<RadioModel[]>();
-  return (
-    <Layout className="flex-grow">
-      <HeroSection />
-      <ProductSection radios={radios} />
-      <RetroExperienceSection />
-      <CTASection />
-      <TestimonialSection />
-      <ContactForm />
-    </Layout>
-  );
+	const radios = useLoaderData<RadioModel[]>();
+	return (
+		<Layout className="flex-grow">
+			<HeroSection />
+			<ProductSection radios={radios} />
+			<RetroExperienceSection />
+			<CTASection />
+			<TestimonialSection />
+			<ContactForm />
+		</Layout>
+	);
 }
